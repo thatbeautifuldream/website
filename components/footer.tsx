9b6ebaea82796631848c794594380966d93345a4
thatbeautifuldream <milind.mishra4@gmail.com>
2025-07-05 08:18:32 +0530

    fix: update footer sub styles

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,9 +1,10 @@
 import { Link } from './link';
 
+
 export const Footer = () => (
   <footer className="text-foreground-lighter text-sm leading-relaxed">
     <p>&copy; {new Date().getFullYear()} Milind Mishra. All rights reserved.</p>
-    <p>
+    <p className="text-xs">
       View the{' '}
       <Link href="https://github.com/thatbeautifuldream/website">
         source code
