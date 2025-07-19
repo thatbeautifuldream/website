3ae58a92c26fab7845c79075cdbaefc0bd8cc3d1
Milind Mishra <milind.mishra4@gmail.com>
2025-07-19 06:14:49 +0530

    feat: setup dev and prod orpc link

--- lib/orpc.ts
+++ lib/orpc.ts
@@ -4,8 +4,10 @@ import type { RouterClient } from "@orpc/server";
 import { createTanstackQueryUtils } from "@orpc/tanstack-query";
 import type { router } from "@/router";
 
+const isDev = process.env.NODE_ENV === "development";
+
 const link = new RPCLink({
-  url: "http://localhost:3000/rpc",
+  url: isDev ? "http://localhost:3000/rpc" : "https://milindmishra.com/rpc",
 });
 
 export const client: RouterClient<typeof router> = createORPCClient(link);
