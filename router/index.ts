d5beaaee60758d02866fac32775577ea0b6cfae1
Milind Mishra <milind.mishra4@gmail.com>
2025-07-19 16:26:48 +0530

    feat: misc organisation

--- router/index.ts
+++ router/index.ts
@@ -1,15 +1,5 @@
 import { os } from "@orpc/server";
-import { z } from "zod";
-
-const helloWorld = os
-  .input(
-    z.object({
-      name: z.string(),
-    })
-  )
-  .handler(({ input }) => {
-    return `Hello, ${input.name}!`;
-  });
+import { helloWorld } from "./hello";
 
 export const router = os.router({
   hello: helloWorld,
