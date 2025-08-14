4ac11fcea5c4121bdb495fd7fe4da00a715b74d2
Milind Mishra <milind.mishra4@gmail.com>
2025-08-15 05:14:53 +0530

    feat: payload upload thing storage init (#3)

--- payload-collections/index.ts
+++ payload-collections/index.ts
@@ -1,4 +1,5 @@
 import { Users } from "./user";
 import { Posts } from "./posts";
+import { Media } from "./media";
 
-export const collections = [Users, Posts];
+export const collections = [Users, Posts, Media];
