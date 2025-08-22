0ce4841198153d574c6e89194415785c2a82bcfe
Milind Mishra <milind.mishra4@gmail.com>
2025-08-23 03:46:36 +0530

    feat: preserve redirects

--- next.config.ts
+++ next.config.ts
@@ -3,49 +3,66 @@ import { withContentCollections } from "@content-collections/next";
 import { withPayload } from "@payloadcms/next/withPayload";
 import type { NextConfig } from "next";
 
+const redirectsConfig = async () => {
+  return [
+    {
+      source: "/resume",
+      destination: "https://resume.milind.app",
+      permanent: true,
+    },
+    {
+      source: "/json",
+      destination: "https://json.milind.app",
+      permanent: true,
+    },
+    {
+      source: "/canvas",
+      destination: "https://canvas.milind.app",
+      permanent: true,
+    },
+    {
+      source: "/notes",
+      destination: "https://notes.milind.app",
+      permanent: true,
+    },
+    {
+      source: "/linkedin",
+      destination: "https://linkedin.com/in/mishramilind",
+      permanent: true,
+    },
+    {
+      source: "/github",
+      destination: "https://github.com/thatbeautifuldream",
+      permanent: true,
+    },
+    {
+      source: "/x",
+      destination: "https://x.com/milindmishra_",
+      permanent: true,
+    },
+    {
+      source: "/instagram",
+      destination: "https://instagram.com/thatbeautifuldream",
+      permanent: true,
+    },
+    {
+      source: "/dm",
+      destination: "https://wa.me/919631333128",
+      permanent: true,
+    },
+  ];
+};
+
 const nextConfig: NextConfig = {
   experimental: {
     reactCompiler: false,
   },
-  redirects: async () => {
-    return [
-      {
-        source: "/resume",
-        destination: "https://resume.milind.app",
-        permanent: true,
-      },
-      {
-        source: "/json",
-        destination: "https://json.milind.app",
-        permanent: true,
-      },
-      {
-        source: "/notes",
-        destination: "https://notes.milind.app",
-        permanent: true,
-      },
-      {
-        source: "/linkedin",
-        destination: "https://linkedin.com/in/mishramilind",
-        permanent: true,
-      },
-      {
-        source: "/github",
-        destination: "https://github.com/thatbeautifuldream",
-        permanent: true,
-      },
-      {
-        source: "/x",
-        destination: "https://x.com/milindmishra_",
-        permanent: true,
-      },
-      {
-        source: "/instagram",
-        destination: "https://instagram.com/thatbeautifuldream",
-        permanent: true,
-      },
-    ];
-  },
+  redirects: redirectsConfig,
 };
 
-export default withPayload(withContentCollections(nextConfig));
+export default withPayload(
+  withContentCollections({
+    ...nextConfig,
+    redirects: redirectsConfig,
+  })
+);
