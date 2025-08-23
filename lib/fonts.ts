dee555a758faf84996a7f692e1883cae7c8ea8d2
Milind Mishra <milind.mishra4@gmail.com>
2025-08-23 07:21:26 +0530

    feat: update typography

--- lib/fonts.ts
+++ lib/fonts.ts
@@ -1,45 +1,28 @@
-import localFont from "next/font/local";
+import { Geist, Geist_Mono, Instrument_Sans } from "next/font/google";
 
-const proximaVara = localFont({
-  src: "../public/fonts/proxima_vara.woff2",
-  variable: "--font-proxima-vara",
-  display: "swap",
+export const geist = Geist({
+  subsets: ["latin"],
+  variable: "--font-geist",
 });
 
-const fkGrotesk = localFont({
-  src: "../public/fonts/fk_grotesk.woff2",
-  variable: "--font-fk-grotesk",
-  display: "swap",
+export const geistMono = Geist_Mono({
+  subsets: ["latin"],
+  variable: "--font-geist-mono",
 });
 
-const fkGroteskNeue = localFont({
-  src: "../public/fonts/fk_grotesk_neue.woff2",
-  variable: "--font-fk-grotesk-neue",
-  display: "swap",
-});
-
-const berkeleyMono = localFont({
-  src: "../public/fonts/berkeley_mono.woff2",
-  variable: "--font-berkeley-mono",
-  display: "swap",
-});
-
-const ppEditorialNew = localFont({
-  src: "../public/fonts/pp_editorial_new_variable.woff2",
-  variable: "--font-pp-editorial-new",
-  display: "swap",
+export const instrumentSans = Instrument_Sans({
+  subsets: ["latin"],
+  variable: "--font-instrument-sans",
 });
 
 export const sans = {
-  proximaVara,
-  fkGrotesk,
-  fkGroteskNeue,
+  instrumentSans,
 };
 
 export const mono = {
-  berkeleyMono,
+  geistMono,
 };
 
 export const serif = {
-  ppEditorialNew,
+  geist,
 };
