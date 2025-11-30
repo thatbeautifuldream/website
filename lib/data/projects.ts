7f551333f3bb07ef3a15d077094fafbb1a25b579
Milind Mishra <milind.mishra4@gmail.com>
2025-12-01 03:48:43 +0530

    feat: update saral dashboard project

--- lib/data/projects.ts
+++ lib/data/projects.ts
@@ -22,6 +22,15 @@ export const PROJECTS: TProject[] = [
     tags: ["Next.js", "React Query", "LLMs", "RAG"],
   },
   {
+    slug: "saral-admin-dashboard",
+    title: "Saral's Admin Dashboard",
+    description:
+      "Revamped Saral's old admin dashboard into beautiful looking one, which reduced onboarding and insight delivery time significantly.",
+    video: "https://cdn.milind.app/media/projects/saral-admin-dash-revamp.webm",
+    date: new Date("2025-01-01"),
+    tags: ["Tanstack Router", "Framer Motion", "Data Aggregation"],
+  },
+  {
     slug: "models-surf",
     title: "Models Surf",
     description: "Surf AI models and filter through their capablities!",
